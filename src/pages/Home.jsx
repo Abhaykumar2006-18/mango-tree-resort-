a57476da1a01dc52362e59d31b19b67f47d18b28
Abhaykumar-web <theukgaming425@gmail.com>
2026-07-17 23:04:57 +0530

    Update home page background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,27 @@ import  Form from '../components/Form'
 import RoomSample from '../components/RoomSample'
 import Hero from '../components/Hero'
 import Last from '../components/Last'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 
 
 
 const Home = () => {
+   const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
    
   return (
     <>
-        <div style={{
-    backgroundImage: "url('/img2.jpeg')",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    minHeight: "100vh",
-  }} className="min-h-[100vh] w-[100%] bg-cover bg-no-repeat bg-center bg-[url('/img2.jpeg')]  md:bg-[image:linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.5),transparent),url('/img3.jpeg')]">
+        <div style={{ backgroundImage: isMobile ? "url('/img2.jpeg')"  : "linear-gradient(to right, rgba(0,0,0,0.5), rgba(0,0,0,0.5), transparent), url('/img3.jpeg')", backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }} className="min-h-[100vh] w-[100%] bg-cover bg-no-repeat bg-center bg-[url('/img2.jpeg')]  md:bg-[image:linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.5),transparent),url('/img3.jpeg')]">
              
 
             <Hero/>
