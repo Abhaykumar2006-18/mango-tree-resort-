dd08eba8854f21e3d8136e4d858c23eb215f21c5
Abhaykumar-web <theukgaming425@gmail.com>
2026-07-17 23:00:10 +0530

    Updated UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,13 @@ const Home = () => {
    
   return (
     <>
-        <div className="min-h-[100vh] w-[100%] bg-cover bg-no-repeat bg-center bg-[url('/img2.jpeg')]  md:bg-[image:linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.5),transparent),url('/img3.jpeg')]">
+        <div style={{
+    backgroundImage: "url('/img2.jpeg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+  }} className="min-h-[100vh] w-[100%] bg-cover bg-no-repeat bg-center bg-[url('/img2.jpeg')]  md:bg-[image:linear-gradient(to_right,rgba(0,0,0,0.5),rgba(0,0,0,0.5),transparent),url('/img3.jpeg')]">
              
 
             <Hero/>
